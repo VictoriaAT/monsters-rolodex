1a7db6245e130163a60e09a127471c512fe6b7ff
victoriaat <victoriaalomoto@gmail.com>
2020-05-19 12:49:43 -0500

    Updates

---
+++ precache-manifest.35fb2171947c9d0333d8f446fe938a87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5bd7154cf5f3b5d611020f552e15311",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cf71cf74bf8d887ff271",
+    "url": "/monsters-rolodex/static/css/main.0b846653.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf71cf74bf8d887ff271",
+    "url": "/monsters-rolodex/static/js/main.e4892770.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
